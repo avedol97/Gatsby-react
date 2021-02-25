5c5ab40d83d06f0a25d6b0eb0f9f1bfa1efa42b1
krystian <krystian.cwioro@o2.pl>
2021-02-25 17:54:55 +0100

    'mdx'

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,11 +1,34 @@
 import React from "react"
+import {graphql} from 'gatsby'
 
-
-const ArticlesPage = () => (
+const ArticlesPage = ({data}) => (
     <div>
         <h1>Blog Page</h1>
         <p>Welcome to Blog page</p>
+        {data.allMdx.nodes.map(item => (
+            <>
+            <h2>{item.frontmatter.title}</h2>
+            <p>{item.frontmatter.author}</p>
+                <p>{item.excerpt}</p>
+            </>
+        ) )}
     </div>
 )
+
+
+export const query = graphql`
+{
+  allMdx{
+    nodes{
+     frontmatter {
+        author
+        slug
+        title
+  }
+    excerpt(pruneLength:50)
+  }
+  }
+  }
+`;
 
 export default ArticlesPage;
